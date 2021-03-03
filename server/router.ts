7973d6cddc63e00dccb842bfc12c963b3fa323c1
Hector Kennedy-Dyson <hectorekd@gmail.com>
2021-03-03 20:10:45 +0000

    chore: app created with nav bar for two pages

--- server/router.ts
+++ server/router.ts
@@ -2,6 +2,6 @@ import express from 'express';
 const router = express.Router();
 import controller from './controllers/moves';
 
-router.get('/allMoves', controller.getAllMoves);
+router.get('/codeofpoints', controller.getAllMoves);
 
 export default router;
